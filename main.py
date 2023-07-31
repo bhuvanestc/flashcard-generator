c708a9fd205f1c13d1bc26a7aefd82163af58789
bhuvan <bhuvanapraap@gmail.com>
2023-07-31 16:40:18 +0200

    canvas

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
+from tkinter import *
+
+
+
 BACKGROUND_COLOR = "#B1DDC6"
+window = Tk()
+window.title("Flash Card Generator")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400,265,image=card_front_img)
+canvas.grid(row=0,column=0)
+
+
+window.mainloop()
+
+
+
+
+
+
+
+
+
+
+def new_card():
+    global current_word
 
