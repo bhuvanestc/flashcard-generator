d5068b77b7f868e8ccec7b675f5a2fb01636878f
bhuvan <bhuvanapraap@gmail.com>
2023-07-31 17:00:31 +0200

    user interface created

--- main.py
+++ main.py
@@ -9,7 +9,19 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400,265,image=card_front_img)
-canvas.grid(row=0,column=0)
+canvas.create_text(400,150, text="Title", font=("Ariel",40,"italic"))
+canvas.create_text(400,265, text="word", font=("Ariel",40,"bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0,column=0,columnspan=2)
+
+wrong_image = PhotoImage(file="images/wrong.png")
+wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button.grid(row=1,column=0)
+
+
+right_image = PhotoImage(file="images/right.png")
+right_button = Button(image=right_image, highlightthickness=0)
+right_button.grid(row=1,column=1)
 
 
 window.mainloop()
@@ -23,6 +35,4 @@ window.mainloop()
 
 
 
-def new_card():
-    global current_word
 
